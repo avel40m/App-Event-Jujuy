b383b7507508f5615e342c35e9c4c5685631b75a
Flores Juan Gabriel <gabriel.flores.tec@gmail.com>
2023-04-16 19:38:20 -0300

    Add festivals cards

--- src/screens/Festival/FestivalScreen.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native"
-
-export const FestivalScreen = () => {
-    return (
-        <View>
-            <Text>Festival</Text>
-        </View>
-    )
-}
---
+++ src/screens/FestivalDetailScreen/FestivalDetailScreen.styles.js
@@ -0,0 +1,64 @@
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight,
+    },
+    imageContainer: {
+        height: 300,
+    },
+    image: {
+        width: Dimensions.get("screen").width
+    },
+    textContainer: {
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textTitleCorto: {
+        fontSize: 22,
+        fontWeight: '700',
+        color: '#070A52'
+    },
+    textTitleLargo: {
+        fontSize: 18,
+        fontWeight: '700',
+        color: '#070A52'
+    },
+    description: {
+        marginTop: 12,
+        fontSize: 14,
+        textAlign: 'justify',
+        width: 270
+    },
+    location: {
+        flexDirection: 'column',
+        marginTop: 22,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 24
+    },
+    lugar: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        gap: 2
+    },
+    locationTitle: {
+        fontSize: 18,
+        fontWeight: '500',
+        color: 'gray'
+    },
+    hours: {
+        flexDirection: 'row',
+        gap: 70
+    },
+    timer: {
+        flexDirection: 'row',
+        gap: 10
+    },
+    map: {
+        height: 250,
+        marginVertical: 20,
+        borderRadius: 10
+    }
+})
