57a39d12c47a8b7a9b191cd57116f1c63cae2110
Flores Juan Gabriel <gabriel.flores.tec@gmail.com>
2023-04-16 17:32:28 -0300

    creando la rama

--- src/api/comment.js
+++ src/api/comment.js
@@ -5,23 +5,23 @@ export async function savedComment(user, title, comentario) {
         const comment = { user, title, comentario }
         const comments = await AsyncStorage.getItem('comment')
         console.log(comments);
-        
-        if(comments !== null) {
+
+        if (comments !== null) {
             const commentArray = JSON.parse(comments);
             commentArray.push(comment);
-            await AsyncStorage.setItem('comment',JSON.stringify(commentArray))
+            await AsyncStorage.setItem('comment', JSON.stringify(commentArray))
         } else {
-            await AsyncStorage.setItem('comment',JSON.stringify([comment]))
+            await AsyncStorage.setItem('comment', JSON.stringify([comment]))
         }
     } catch (error) {
         console.log(error);
     }
 }
 
-export async function recoverComment(){
+export async function recoverComment() {
     try {
         const comment = await AsyncStorage.getItem('comment');
-        if(comment !== null){
+        if (comment !== null) {
             return JSON.parse(comment)
         }
     } catch (error) {
